6d841e603ac6f06b8213c1e15622226214fcf6a3
Geetesh Gupta <gupta.15@iitj.ac.in>
2019-02-13 19:16:49 +0530

    Add gcounter.py for manual_testing

---
+++ tests/manual/gcounter.py
@@ -0,0 +1,43 @@
+import sys
+
+sys.path.insert(0, sys.argv[1])
+
+import uuid
+from gcounter import GCounter
+from node import Node
+
+# Create nodes
+node1 = Node(uuid.uuid4())
+node2 = Node(uuid.uuid4())
+
+# Create a GCounter
+gc1 = GCounter(uuid.uuid4())
+
+# Add nodes to gc1
+gc1.add_new_node(node1.id)
+gc1.add_new_node(node2.id)
+
+# Increment gc1 values for each node
+gc1.inc(node1.id)
+gc1.inc(node1.id)
+gc1.inc(node2.id)
+
+# Create another GCounter
+gc2 = GCounter(uuid.uuid4())
+
+# Add nodes to gc2
+gc2.add_new_node(node1.id)
+gc2.add_new_node(node2.id)
+
+# Increment gc2 values for each node
+gc2.inc(node1.id)
+gc2.inc(node2.id)
+gc2.inc(node2.id)
+gc2.inc(node2.id)
+
+# Display counters
+gc1.display()
+gc2.display()
+
+# Merge gc2 with gc1
+gc2.merge(gc1)
