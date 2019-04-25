12988e239fd6ee80f383ff88f556673556973890
Geetesh Gupta <gupta.15@iitj.ac.in>
2019-04-25 12:52:59 +0530

    Update package name

--- tests/set_sys_path.py
+++ tests/set_sys_path.py
@@ -2,5 +2,5 @@ import os
 import sys
 
 root_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-src_path = root_path + "/src"
+src_path = root_path + "/py3crdt"
 sys.path.insert(0, src_path)
